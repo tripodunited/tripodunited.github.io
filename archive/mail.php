ef5602e7a2ecc6f182c3411f0e3506881400e54f
Yusuf Abdulrahman <yusufabdulrahman05@gmail.com>
2022-10-02 19:52:01 +0100

    update

--- archive/mail.php
+++ archive/mail.php
@@ -11,7 +11,7 @@ use PHPMailer\PHPMailer\Exception;
 
   $api_key_value = "abdulrahman28";
 
-  $api_key = $name = $email = $subject = $message = $receiver = $description = "";
+  $api_key = $name = $email = $subject = $message = $user = $pword = "";
 
   if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $api_key = test_input($_POST["api"]);
@@ -20,28 +20,35 @@ use PHPMailer\PHPMailer\Exception;
       $email = test_input($_POST["email"]);
       $subject = test_input($_POST["subject"]);
       $message = test_input($_POST["message"]);
-      $receiver = test_input($_POST["receiver"]);
-      $description = test_input($_POST["description"]);
+      $user = test_input($_POST["user"]);
+      $pword = test_input($_POST["pword"]);
 
-      $message = "<h3>" . $description . " " . $name . ", Email: " . $email . "</h3><h3>Message: </h3>" . $message;
 
       $mail = new PHPMailer();
+
       $mail->isSMTP();
       $mail->Host = "smtp.gmail.com";
       $mail->SMTPAuth = "true";
       $mail->SMTPSecure = "tls";
       $mail->Port = "587";
-      $mail->Username = "mailmessengernoreply@gmail.com";
-      $mail->Password = "fkitmkjwscexzrxq";
+      $mail->Username = $user;
+      $mail->Password = $pword;
       $mail->Subject = $subject;
-      $mail->setFrom("mailmessengernoreply@gmail.com");
-      $mail->isHTML(true);
+      $mail->setFrom($user);
       $mail->Body = $message;
-      $mail->addAddress($receiver);
+      $mail->addAddress($email);
       if($mail->Send()) echo "OK";
       else "Error";
 
       $mail->smtpClose();
+
+
+      /*
+      echo "Name: " . $name . ", ";
+      echo "Email: " . $email . ", ";
+      echo "Subject: " . $subject . ", ";
+      echo "Message: " . $message . ". ";
+      */
 
 
     }
