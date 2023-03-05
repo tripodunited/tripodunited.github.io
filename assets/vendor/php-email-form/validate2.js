1b6c1f93bb12520bc053673876de3e193ac5a0b9
Yusuf Abdulrahman <yusufabdulrahman05@gmail.com>
2023-03-05 19:44:06 +0100

    update

--- assets/vendor/php-email-form/validate2.js
+++ assets/vendor/php-email-form/validate2.js
@@ -4,6 +4,7 @@
 * Author: BootstrapMade.com
 */
 (function () {
+
   "use strict";
 
   let forms = document.querySelectorAll('.php-email-form');
@@ -50,11 +51,40 @@
   });
 
   function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
+
+
+  // make post request
+  fetch('https://rdenv.ml/mail.php', {
+    method: 'POST',
+    body: formData,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+})
+.then(response =>  {
+  if( response.ok ) {
+    return response.text()
+  } else {
+    throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+  }
+})
+
+
+.then(html => console.log(html))
+.catch(err => console.error(err));
+
+
+    /*
+    fetch('https://rdenv.ml/mail.php', {
       method: 'POST',
       body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+     // headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
+
+
     .then(response => {
       if( response.ok ) {
         return response.text()
@@ -62,6 +92,7 @@
         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
       }
     })
+
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
       if (data.trim() == 'OK') {
@@ -71,9 +102,10 @@
         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
       }
     })
+
     .catch((error) => {
       displayError(thisForm, error);
-    });
+    });*/
   }
 
   function displayError(thisForm, error) {
